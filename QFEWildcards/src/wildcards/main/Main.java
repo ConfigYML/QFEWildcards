18f196102c2bc7bff7376ad46901374f20062445
Jonabo <jonafred.boss@gmail.com>
2018-10-11 21:38:43 +0200

    Added JavaDoc for better understanding of things happening.

--- QFEWildcards/src/wildcards/main/Main.java
+++ QFEWildcards/src/wildcards/main/Main.java
@@ -3,7 +3,16 @@ package wildcards.main;
 import wildcards.connector.MySQL;
 
 public class Main {
-	
+	/**
+	 * This method establishes a connection to the MySQL database and enables all other Methods to be executed correct.
+	 *
+	 * @param host	 Sets the host for the MySQL connection
+	 * @param port	 Sets the port for the MySQL connection
+	 * @param database	 Sets the database for the MySQL connection
+	 * @param username	 Sets the username for the MySQL connection
+	 * @param password	 Sets the password for the MySQL connection
+	 * @param autoReconnect	 Sets whether to automatically reconnect to the database
+	 */
 	public void establishConnection(String host, int port, String database, String username, String password, boolean autoReconnect) {
 		MySQL mysql = new MySQL(host, port, database, username, password, autoReconnect);
 		mysql.connectToDB();

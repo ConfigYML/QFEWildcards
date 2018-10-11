ff2daac8996e314828e15a4c1dd6e361812a016e
Jonabo <jonafred.boss@gmail.com>
2018-10-11 21:52:45 +0200

    Added DatabaseHelper for some basic tasks.

---
+++ QFEWildcards/src/wildcards/connector/DatabaseHelper.java
@@ -0,0 +1,36 @@
+package wildcards.connector;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DatabaseHelper {
+	
+	private Connection connection;
+	/**
+	 * This method creates the table for the Wildcards itself.
+	 * @param con Connection from the MySQL Connector
+	 */
+	public DatabaseHelper(Connection con) {
+		connection = con;
+		
+		try {
+			PreparedStatement ps = connection.prepareStatement("create table if not exists Wildcards(WildcardID not null auto_increment, Token varchar(256), Used tinyint,  primary key(WildcardID));");
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * In case someone would like to drop the table automatically, this method should be called. Otherwise this method is very dangerous!
+	 */
+	public void dropTable() {
+		try {
+			PreparedStatement ps = connection.prepareStatement("drop table Wildcards;");
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
